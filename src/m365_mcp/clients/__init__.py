f68353fe8473c21574e4cf18630009858fc6e1ec
Paul Turek <paul.turek@bolthousefresh.com>
2026-03-02 09:59:35 -0800

    feat: export PowerBIClient from clients package

--- src/m365_mcp/clients/__init__.py
+++ src/m365_mcp/clients/__init__.py
@@ -2,12 +2,15 @@
 
 Provides async HTTP clients for:
 - Microsoft Graph API (graph.microsoft.com)
+- Power BI REST API (api.powerbi.com)
 """
 
 from .graph_client import GraphClient, GraphAPIError, AuthenticationRequiredError
+from .powerbi_client import PowerBIClient
 
 __all__ = [
     "GraphClient",
     "GraphAPIError",
     "AuthenticationRequiredError",
+    "PowerBIClient",
 ]
